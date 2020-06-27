351b03c5a07a54d5c392554bc94feb23327c2cbd
TechFring <danilo030920@gmail.com>
2020-06-26 23:49:52 -0300

    adicionada a requisição dos dados do perfil

--- js/requests.js
+++ js/requests.js
@@ -1,3 +1,27 @@
 function searchProfile(name) {
-    console.log(name);
+    $.ajax({
+        type: 'get',
+        url: `https://api.github.com/users/${name}`,
+        dataType: 'json',
+        beforeSend: () => {
+            $('#load').removeClass('d-none');
+            $('#data').addClass('d-none');
+        },
+        success: data => {
+            console.log(data);
+
+            $('#load').addClass('d-none')
+            $('#data').removeClass('d-none');
+            $('#data').find('.card-header').text(data.login);
+            $('#data').find('img').attr('src', data.avatar_url);
+            $('#data').find('span:first').text(`${data.followers} followers`);
+            $('#data').find('span:odd').text(`${data.following} followers`);
+            $('#data').find('span:last').text(`${data.public_repos} repositories`);
+        },
+        error: () => {
+            $('#load').addClass('d-none');
+            $('#data').addClass('d-none');
+            alertify.error('Profile not found');
+        }
+    });
 }
