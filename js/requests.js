5ae9adff2c05884c94f9db80f2520e215e0833c4
TechFring <danilo030920@gmail.com>
2020-06-27 13:31:49 -0300

    refatoração de código

--- js/requests.js
+++ js/requests.js
@@ -6,29 +6,39 @@ function searchProfile(username) {
         beforeSend: () => {
             toggleClassSpinner();
             $('#spn').attr('role', 'status');
-            
-            $('#loadData').removeClass('d-none');
-            $('#data').addClass('d-none');
+            $('#data').append('<img src="images/skeleton.gif" class="img-fluid">');
         },
         success: data => {
+            console.log(data);
             toggleClassSpinner();
             $('#spn').attr('role', '');
 
-            $('#loadData').addClass('d-none')
-            $('#data').removeClass('d-none');
+            $('#data').empty();
 
-            $('#data').find('.card-header').text(data.login);
-            $('#data').find('img').attr('src', data.avatar_url);
-            $('#data').find('span:first').text(`${data.followers} followers`);
-            $('#data').find('span:odd').text(`${data.following} following`);
-            $('#data').find('span:last').text(`${data.public_repos} repositories`);
+            $('#data').append(
+                `<div class="card text-center">
+                    <div class="card-header text-muted">
+                        <a href="${data.html_url}" target="_blank">
+                            <span>${data.login} <i class="fas fa-link"></i></span>                    
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="${data.html_url}" target="_blank">
+                            <img src="${data.avatar_url}" class="img-fluid avatar">
+                        </a>
+                    </div>
+                    <div class="card-footer">
+                        <span><i class="fas fa-user-friends"></i> ${data.followers} followers |</span>
+                        <span><i class="fas fa-people-arrows"></i> ${data.following} following |</span>
+                        <span><i class="fas fa-sort-amount-up"></i> ${data.public_repos} repositories</span>
+                    </div>
+                </div>`
+            );
         },
         error: () => {
             toggleClassSpinner();
             $('#spn').attr('role', '');
-
-            $('#loadData').addClass('d-none');
-            $('#data').addClass('d-none');
+            $('#data').empty();
             alertify.error('Profile not found');
         }
     });
@@ -40,18 +50,37 @@ function searchRepositories(username) {
         url: `https://api.github.com/users/${username}/repos`,
         dataType: 'json',
         beforeSend: () => {
-            $('#loadRepositories').removeClass('d-none');
-            $('#repositories').addClass('d-none');
+            $('#repositories').append('<img src="images/skeleton.gif" class="img-fluid">');
         },
         success: data => {
-            console.log(data);
+            $('#repositories').empty();
 
-            $('#loadRepositories').addClass('d-none');
-            $('#repositories').removeClass('d-none');
+            data.forEach((v, i) => {
+                if (i < 6) {
+                    $('#repositories').append(
+                        `<div class="col-md-6 col-lg-4 mb-4">
+                            <div class="card">
+                                <div class="card-header text-center">
+                                    <i class="fas fa-star"></i> ${v.stargazers_count}
+                                </div>
+                                <div class="card-body">
+                                    <p>Description: ${v.description}</p>
+                                    <p>Language: ${v.language}</p>
+                                    <a class="btn btn-outline-dark" href="${v.html_url}" target="_blank">Go to repository</a>
+                                </div>
+                                <div class="card-footer text-center">
+                                    <span class="text-muted">${v.name}</span>
+                                </div>
+                            </div>
+                        </div>`
+                    );
+                }
+            });
+
+            $('#repositories').parent().prepend('<p class="lead">Repositories</p>');
         },
         error: error => {
-            $('#loadRepositories').addClass('d-none');
-            $('#repositories').removeClass('d-none');
+            $('#repositories').empty();
         }
     });
 }
